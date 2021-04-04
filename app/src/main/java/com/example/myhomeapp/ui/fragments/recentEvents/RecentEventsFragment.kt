5f1605777d42b00a83d01a017d0263057105e367
nik fed <nikfed142@gmail.com>
2021-04-04 14:52:57 +0300

    recent events page

--- app/src/main/java/com/example/myhomeapp/ui/fragments/recentEvents/RecentEventsFragment.kt
+++ app/src/main/java/com/example/myhomeapp/ui/fragments/recentEvents/RecentEventsFragment.kt
@@ -1,20 +1,19 @@
 package com.example.myhomeapp.ui.fragments.recentEvents
 
-import com.example.myhomeapp.R
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.myhomeapp.R
 
-class FirstFragment : Fragment() {
+class RecentEventsFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
