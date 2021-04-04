b927ab76fb84f762d6f6e706fbb7b9d75441a13e
nik fed <nikfed142@gmail.com>
2021-04-04 14:16:51 +0300

    new values and layouts

--- app/src/main/java/com/example/myhomeapp/di/ViewModelFactoryModule.kt
+++ app/src/main/java/com/example/myhomeapp/di/ViewModelFactoryModule.kt
@@ -3,29 +3,28 @@ package com.example.myhomeapp.di
 //import com.example.myhomeapp.ui.fragments.converter.ConverterViewModel
 //import com.example.myhomeapp.ui.fragments.feed.NewsFeedViewModel
 //import com.example.myhomeapp.ui.fragments.search.NewsSearchViewModel
+//import dagger.Binds
+//import dagger.Module
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.myhomeapp.ui.fragments.eventHistory.EventHistoryViewModel
 import com.example.myhomeapp.ui.fragments.recentEvents.RecentEventsViewModel
 import com.example.myhomeapp.utils.viewmodel.ViewModelProviderFactory
-import dagger.Binds
-import dagger.Module
-import dagger.multibindings.IntoMap
 
-@Module
+//@Module
 abstract class ViewModelFactoryModule {
 
-    @Binds
+    //@Binds
     internal abstract fun bind(factory: ViewModelProviderFactory): ViewModelProvider.Factory
 
-    @Binds
-    @IntoMap
+    //@Binds
+    //@IntoMap
     //@ViewModelKey(RecentEventsViewModel::class)
     abstract fun bindRecentEventsViewModel(viewModel: RecentEventsViewModel): ViewModel
 
 
-    @Binds
-    @IntoMap
+    //@Binds
+    //@IntoMap
     //@ViewModelKey(EventHistoryViewModel::class)
     abstract fun bindEventHistoryViewModel(viewModel: EventHistoryViewModel): ViewModel
 
--- app/src/main/java/com/example/myhomeapp/ui/placeholder.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myhomeapp.ui
-
-class placeholder {
-}
